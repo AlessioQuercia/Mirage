94468ae5fee606f2df2d206f514e5518f03e0315
Bigbounze <jorgejuanniguezfernandez@gmail.com>
2018-11-24 13:11:52 +0100

    Create CheckGround.cs

---
+++ CheckGround.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+    private PlayerControl player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponentInParent<PlayerControl>();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }
+    }
+}
