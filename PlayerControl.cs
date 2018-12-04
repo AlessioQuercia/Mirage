2a13baf0b60be7774e91364e5cdc7fea9e35f9bc
Alessio Quercia <alessioquercia4@gmail.com>
2018-12-04 12:05:02 +0100

    Fixed ladder interaction in PlayerControl

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -8,7 +8,7 @@ public class PlayerControl : MonoBehaviour {
     public float speed = 10f;
     public float maxSpeed = 1f;
     public float jumpPower = 6.5f;
-    public float climbSpeed;
+    public float climbSpeed = 3;
     public float climbVelocity;
     public float gravityStore;
 
@@ -34,7 +34,7 @@ public class PlayerControl : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
-		floorCollider = GameObject.Find("Floor1").GetComponent<BoxCollider2D>();
+		floorCollider = GameObject.Find("Floor").GetComponent<BoxCollider2D>();
 		
 	    gravityStore = rb2d.gravityScale;
 	}
@@ -45,69 +45,72 @@ public class PlayerControl : MonoBehaviour {
         anim.SetBool("grounded", grounded);
         anim.SetBool("running", run);
 
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) ||
-            Input.GetKeyDown(KeyCode.W)) && grounded && !moveObject)
-        {
-            jump = true;
-        }
-
-        if (!moveObject && Input.GetKeyDown(KeyCode.LeftShift))// && !onWall)
-        {
-            run = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            run = false;
-        }
-
-	    if (onLadder && !moveObject && !onMoveableObject && Input.GetKeyDown(KeyCode.E))
-	    {
-	        climb = true;
-		    floorCollider.enabled = false;
-	    }
-
-		if (!onLadder)
-		{
-			climb = false;
-			floorCollider.enabled = true;
-		}
-		
 	    if (climb)
 	    {
 	        rb2d.gravityScale = 0f;
 	        climbVelocity = climbSpeed * Input.GetAxisRaw("Vertical");
 
 	        rb2d.velocity = new Vector2(rb2d.velocity.x, climbVelocity);
+		    
+		    if (!onLadder || Input.GetKeyDown(KeyCode.E))
+		    {
+			    climb = false;
+			    floorCollider.enabled = true;
+		    }
 	    }
 	    else
 	    {
 	        rb2d.gravityScale = gravityStore;
-	    }
+		    
+		    if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) ||
+		         Input.GetKeyDown(KeyCode.W)) && grounded && !moveObject)
+		    {
+			    jump = true;
+		    }
 
-        if (run)
-        {
-            maxSpeed = 4;
-        }
-        else if (moveObject)
-        {
-	        maxSpeed = 1f;
-        }
-        else
-        {
-            maxSpeed = 2f;
-        }
+		    if (!moveObject && Input.GetKeyDown(KeyCode.LeftShift))// && !onWall)
+		    {
+			    run = true;
+		    }
+		    else if (Input.GetKeyUp(KeyCode.LeftShift))
+		    {
+			    run = false;
+		    }
+
+		    if (onLadder && !moveObject && !onMoveableObject && Input.GetKeyDown(KeyCode.E))
+		    {
+			    climb = true;
+			    floorCollider.enabled = false;
+		    }
+		    
+		    if (run)
+		    {
+			    maxSpeed = 4;
+		    }
+		    else if (moveObject)
+		    {
+			    maxSpeed = 1f;
+		    }
+		    else
+		    {
+			    maxSpeed = 2f;
+		    }
+	    }
     }
 
-    private void FixedUpdate()
-    {
-        float h = Input.GetAxis("Horizontal");
+	private void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
 
-		rb2d.AddForce(Vector2.right * speed * h);
-	    
-	    float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
-	    rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+		if (!climb)
+		{
+			rb2d.AddForce(Vector2.right * speed * h);
 
-	    if (h > 0.1f && !moveObject)
+			float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+			rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+		}
+
+		if (h > 0.1f && !moveObject)
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
