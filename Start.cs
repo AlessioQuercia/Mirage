db7a7e0126abe9bc5a989976b96d1d22b84f4bc8
Bigbounze <jorgejuanniguezfernandez@gmail.com>
2018-12-11 10:03:42 +0100

    Create Start.cs

---
+++ Start.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}
